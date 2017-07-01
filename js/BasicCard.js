37a34a9f0d8dc960bc64390124611e69eab045c9
Cody Childers <cchilder@mail.usf.edu>
2017-07-01 10:35:47 -0500

    format

--- js/BasicCard.js
+++ js/BasicCard.js
@@ -1,12 +1,10 @@
-function BasicCard(front,back){
-    if(this instanceof BasicCard){
-        this.front=front;
-        this.back=back;
+function BasicCard(front, back) {
+    if (this instanceof BasicCard) {
+        this.front = front;
+        this.back = back;
+    } else {
+        return new BasicCard(front, back);
     }
-    else{
-        return new BasicCard(front,back);
-    }
-    
 }
 
-module.exports=BasicCard;
+module.exports = BasicCard;
